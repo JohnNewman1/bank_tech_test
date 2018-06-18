0a5e7f5c8c419c5b2c17f1bf1b0f781eb35e91c0
John Newman <newmanj903@gmail.com>
2018-06-18 13:19:14 +0100

    Test added for add function

--- spec/accountHistorySpec.js
+++ spec/accountHistorySpec.js
@@ -8,4 +8,11 @@ describe('AccountHistory', function(){
   it('Starts with empty array', function(){
     expect(history.log).toEqual([]);
   });
+
+  describe('#add', function(){
+    it('Adds a new transaction object to the log', function(){
+      history._add(100, "", "10-11-2012", 200);
+      expect(history.log[0]).toEqual({date: 10/11/2012, credit: 100, debit: "", balance: 200 })
+    });
+  });
 });
