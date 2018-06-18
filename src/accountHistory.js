a4829532a73d3869d0fe202f525525b058f74030
John Newman <newmanj903@gmail.com>
2018-06-18 13:42:24 +0100

    Test passed for formatting the date

--- src/accountHistory.js
+++ src/accountHistory.js
@@ -3,6 +3,13 @@ function AccountHistory(){
 }
 
 AccountHistory.prototype._add = function(credit, debit, date, balance){
-  var transaction = {date: , credit: credit , debit: debit, balance: balance}
+  var formatDate = _dateFormat(date)
+  var transaction = {date: formatDate, credit: credit, debit: debit, balance: balance}
   this.log.push(transaction)
+}
+
+AccountHistory.prototype._dateFormat = function(date){
+  var formatedDate = date.replace(/-/g, "/");
+  return formatedDate;
+
 }
