c418be13860cdd8c1233ebd685cfb065639666cf
John Newman <newmanj903@gmail.com>
2018-06-19 11:44:54 +0100

    adding type to transaction hash

--- spec/accountHistorySpec.js
+++ spec/accountHistorySpec.js
@@ -8,8 +8,8 @@ describe('AccountHistory', function(){
   describe('#add', function(){
 
     it('Adds a new transaction object to the log', function(){
-      history.add(100, "", "10-11-2012", 200);
-      expect(history.log[0]).toEqual({ date: "10/11/2012", credit: 100.00, debit: "", balance: 200.00 })
+      history.add(100, "", "10-11-2012", 200, "credit");
+      expect(history.log[0]).toEqual({ date: "10/11/2012", credit: 100.00, debit: "", balance: 200.00, type: "credit" })
     });
   });
 });
--- src/printStatement.js
+++ src/printStatement.js
@@ -3,16 +3,8 @@ function PrintStatement(history){
 };
 
 PrintStatement.prototype.print = function() {
-  this._spaceChecker();
   console.log("date || credit || debit || balance\n");
   this.history.log.forEach(function(statement){
     console.log(statement.date + " || " + statement.credit + "|| " + statement.debit + "|| " + Number(statement.balance).toFixed(2) + "\n")
-  });
-}
-
-PrintStatement.prototype._spaceChecker = function() {
-  this.history.log.forEach(function(statement){
-    if(statement.credit != "") {statement.credit = Number(statement.credit).toFixed(2) + " "}
-    if(statement.debit != "") {statement.debit = Number(statement.debit).toFixed(2) + " "}
   });
 }
