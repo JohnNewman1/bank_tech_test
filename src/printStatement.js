21e08419d5fc2cf7d2637b6eb9c5a5d9e55e976d
John Newman <newmanj903@gmail.com>
2018-06-19 13:51:37 +0100

    ternery in private method

--- src/printStatement.js
+++ src/printStatement.js
@@ -1,20 +1,14 @@
 function PrintStatement(history){
   this.history = history
-  this.transaction;
 };
 
 PrintStatement.prototype.print = function() {
   console.log("date || credit || debit || balance\n");
   this.history.log.reverse().forEach(function(statement){
-    this._format(statement)
-    console.log(this.transaction);
+    console.log(statement.date + " ||" + this._format(statement) + " " + Number(statement.balance).toFixed(2) + "\n" );
   }.bind(this));
 }
 
 PrintStatement.prototype._format = function(statement){
-  if (statement.type == "credit") {
-    this.transaction = statement.date + " || " + Number(statement.credit).toFixed(2) + " || || " + Number(statement.balance).toFixed(2) + "\n"
-  } else {
-    this.transaction = statement.date + " || || " + Number(statement.debit).toFixed(2) + " || " + Number(statement.balance).toFixed(2) + "\n"
-  }
+  return (statement.type == "credit") ? ` ${Number(statement.credit).toFixed(2)} || ||` : ` || ${Number(statement.debit).toFixed(2)} ||`;
 }
