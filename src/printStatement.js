b9943b21cc7994c634b0cbfb04637ce4ee3abc36
John Newman <newmanj903@gmail.com>
2018-06-19 12:21:50 +0100

    refactor printStatement

--- src/printStatement.js
+++ src/printStatement.js
@@ -1,14 +1,20 @@
 function PrintStatement(history){
   this.history = history
+  this.transaction;
 };
 
 PrintStatement.prototype.print = function() {
   console.log("date || credit || debit || balance\n");
   this.history.log.forEach(function(statement){
-    if (statement.type == "credit") {
-      console.log(statement.date + " || " + Number(statement.credit).toFixed(2) + " || || " + Number(statement.balance).toFixed(2) + "\n")
-    } else {
-      console.log(statement.date + " || || " + Number(statement.debit).toFixed(2) + " || " + Number(statement.balance).toFixed(2) + "\n")
-    }
-  });
+    this._format(statement)
+    console.log(this.transaction);
+  }.bind(this));
+}
+
+PrintStatement.prototype._format = function(statement){
+  if (statement.type == "credit") {
+    this.transaction = statement.date + " || " + Number(statement.credit).toFixed(2) + " || || " + Number(statement.balance).toFixed(2) + "\n"
+  } else {
+    this.transaction = statement.date + " || || " + Number(statement.debit).toFixed(2) + " || " + Number(statement.balance).toFixed(2) + "\n"
+  }
 }
